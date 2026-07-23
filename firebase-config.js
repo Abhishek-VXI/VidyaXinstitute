0678dfc0a22b00d4cee9f22a40affe4029280cc4
Abhishek-VXI <abhisheksharma01145@gmail.com>
2026-07-23 18:34:10 +0530

    Add Firebase configuration and initialization

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBt7DhcxMVrM2K1_P_2lpwCb7c0COtpMEE",
+  authDomain: "vidyaxinstitute.firebaseapp.com",
+  projectId: "vidyaxinstitute",
+  storageBucket: "vidyaxinstitute.firebasestorage.app",
+  messagingSenderId: "71130562110",
+  appId: "1:71130562110:web:f56ca6700ee375573e5b6f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
